8deb1b4fb28253207577acc9a5741c072392d493
Bulat Giniyatullin <bulat.giniiatullin@gmail.com>
2018-10-15 19:50:21 +0300

    Add Gateway service

---
+++ GatewayService/src/main/java/ru/itis/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,20 @@
+package ru.itis.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author Bulat Giniyatullin
+ * 15 October 2018
+ */
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+}
